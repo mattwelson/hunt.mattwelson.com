1405c09f35ccb5b20f4a6d54b591b79eda0fa5d5
matt.welson <matt.welson@mtf.co.nz>
2023-04-04 04:55:01 +1200

    Adding homepage clue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { Layout } from "../components/layout";
+import { Clue } from "../components/clue";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <h1 className="font-bond text-primary text-5xl">Happy Birthday Alice</h1>
       <p className="text-secondary text-xl">You're the best</p>
+      <p className="text-secondary text-xl">Sei una madre simpatica!</p>
+      <Clue
+        clue="I am a pirate! I am a sprouter!"
+        secondClue="I am the latest edition in your book collection"
+      />
     </Layout>
   );
 };
